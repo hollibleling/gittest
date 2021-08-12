90e6a9331955c2592eb0b90b1ac46244853f30db
김정수 <jungsookim@gimjeongsuui-MacBookAir.local>
2021-08-12 19:02:14 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wecode24',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-0rq0vjbxl04!f^xz1$7%q)*+)5#sq$(3+-!ixk)dp$ymo=2r&k'
